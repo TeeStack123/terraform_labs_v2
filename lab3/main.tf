b5e99d017c121fcfbe949fae3c228e4f9aad4d84
tahir <tahir_malik@next.co.uk>
2024-12-10 12:39:22 +0000

    Lab3 new

--- lab3/main.tf
+++ lab3/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_virtual_network" "lab3_vnet" {
       name                = "example-network"
         resource_group_name = azurerm_resource_group.RG3.name
         location          = azurerm_resource_group.RG3.location
-        address_space     = ["10.0.0.0/16"]
+        address_space     = ["10.1.0.0/16"]
         dns_servers       = ["10.0.0.4", "10.0.0.5"]
             }
 resource "azurerm_subnet" "lab3_subnet" {
