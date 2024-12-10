8286bf93fc7b4ea6205a0f2f6df1e58e55f572b8
tahir <tahir_malik@next.co.uk>
2024-12-10 15:16:55 +0000

    Lab3 and lab4 new

--- lab4/main.tf
+++ lab4/main.tf
@@ -11,8 +11,10 @@ terraform {
 }
 
 provider "azurerm" {
+  subscription_id = "fac486c1-73b6-4499-b5b0-b8b1357a3f02"
   features {}
 }
+
 
 resource "azurerm_resource_group" "resgrp" {
   name     = "RG4"
@@ -21,29 +23,29 @@ resource "azurerm_resource_group" "resgrp" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "myVNet"
-  resource_group_name = "RG4"
-  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.resgrp.name
+  location            = azurerm_resource_group.resgrp.location
   address_space       = ["10.21.0.0/16"]
 }
 
 resource "azurerm_subnet" "frontend" {
   name                 = "myAGSubnet"
-  resource_group_name  = "RG4"
-  virtual_network_name = "myVNet"
+  resource_group_name  = azurerm_resource_group.resgrp.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.21.0.0/24"]
 }
 
 resource "azurerm_subnet" "backend" {
   name                 = "myBackendSubnet"
-  resource_group_name  = "RG4"
-  virtual_network_name = "myVNet"
+  resource_group_name  = azurerm_resource_group.resgrp.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.21.1.0/24"]
 }
 
 resource "azurerm_public_ip" "pubip" {
   name                = "myAGPublicIPAddress"
-  resource_group_name = "RG4"
-  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.resgrp.name
+  location            = azurerm_resource_group.resgrp.location
   allocation_method   = "Static"
   sku                 = "Standard"
 }
@@ -51,8 +53,8 @@ resource "azurerm_public_ip" "pubip" {
 
 resource "azurerm_application_gateway" "appgtw" {
   name                = "myAppGateway"
-  resource_group_name = "RG4"
-  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.resgrp.name
+  location            = azurerm_resource_group.resgrp.location
 
   sku {
     name     = "Standard_v2"
